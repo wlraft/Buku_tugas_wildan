1fcb70f8014ce3ff8ba05ea3bbc46940ef224384
wlraft <wildanarrafi28@gmail.com>
2023-04-28 18:32:34 +0700

    tugas layout komponen wildan

--- app/src/main/java/com/example/buku_tugas_wildan/MainActivity.java
+++ app/src/main/java/com/example/buku_tugas_wildan/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.latihan_layout);
+        setContentView(R.layout.latihan_komponen);
     }
 }
